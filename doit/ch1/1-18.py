3d3c2b5d8307b608475cb3dcf88d88946101640d
parkchoongho <pchyo92@gmail.com>
2021-08-07 13:11:18 +0900

    doit 책 1장

--- doit/ch1/1-18.py
+++ doit/ch1/1-18.py
@@ -4,4 +4,9 @@ n = int(input('난수의 개수를 입력하세요.: '))
 
 for _ in range(n):
     r = random.randint(10, 99)
-    
+    print(r, end=' ')
+    if r == 13:
+        print('\n프로그램을 중단합니다.')
+        break
+
+print('\n난수 생성을 종료합니다.')
