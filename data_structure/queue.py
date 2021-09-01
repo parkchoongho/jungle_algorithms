f9cc1cf56b908c23d45b81f4cbe2819b294b76cd
parkchoongho <pchyo92@gmail.com>
2021-09-01 15:18:05 +0900

    자료구조 큐 구현

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -1,13 +1,21 @@
 from abc import abstractmethod
 
 
+class NoSuchElement(Exception):
+    pass
+
+
+class WrongIndex(Exception):
+    pass
+
+
 class Node:
-    def __init__(self, val):
+    def __init__(self, val) -> None:
         self.val = val
         self.next = None
         self.prev = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         result = "{ "
         result += str(self.val)
         result += " }"
@@ -24,78 +32,73 @@ class QueueInterface:
         pass
 
     @abstractmethod
-    def search(self, index) -> Node:
+    def search(self, index: int) -> Node:
         pass
 
     @abstractmethod
-    def __str__(self):
+    def peek(self) -> Node or None:
         pass
 
 
-class NoSuchElementException(Exception):
-    pass
-
-
-class WrongIndex(Exception):
-    pass
-
-
 class Queue(QueueInterface):
-    def __init__(self):
+    def __init__(self) -> None:
+        self.size = 0
         self.first = None
         self.last = None
-        self.size = 0
 
     def enqueue(self, node: Node) -> None:
         if self.size == 0:
-            self.first = node
             self.last = node
+            self.first = node
         else:
-            self.last.next = node
             node.prev = self.last
+            self.last.next = node
             self.last = node
         self.size += 1
         return
 
     def dequeue(self) -> Node:
         if self.size == 0:
-            raise NoSuchElementException()
+            raise NoSuchElement()
         if self.size == 1:
             node = self.first
             self.first = None
             self.last = None
         else:
             node = self.first
-            self.first = self.first.next
-            self.first.prev = None
+            next_node = self.first.next
             node.next = None
+            next_node.prev = None
+            self.first = next_node
         self.size -= 1
         return node
 
-    def search(self, index) -> Node:
+    def search(self, index: int) -> Node:
         if index < 0 or index > self.size - 1:
             raise WrongIndex()
+        if self.size == 0:
+            raise NoSuchElement()
         start_index = 0
         node = self.first
         while start_index != index:
-            start_index += 1
             node = node.next
+            start_index += 1
         return node
 
-    def peek(self) -> Node:
+    def peek(self) -> Node or None:
         if self.size == 0:
-            raise NoSuchElementException()
+            raise NoSuchElement()
         return self.first
 
-    def __str__(self):
+    def __str__(self) -> str:
         node = self.first
-        result = "None "
+        result = "None"
         while node is not None:
             result += "<- "
             result += str(node.val)
-            result += " -> "
+            result += " ->"
             node = node.next
-        result += "None"
+        result += 'None'
         return result
 
 
