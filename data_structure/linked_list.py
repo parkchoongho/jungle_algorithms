486411d5aa0df6afe69216b209fd1e161739676a
parkchoongho <pchyo92@gmail.com>
2021-09-01 15:17:50 +0900

    자료구조 링크드리스트 구현

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -1,14 +1,19 @@
 from abc import abstractmethod
 
 
-class NoSuchElementException(Exception):
+class NoSuchElement(Exception):
     pass
 
 
 class Node:
-    def __init__(self, val):
+    def __init__(self, val) -> None:
         self.val = val
         self.next = None
+
+    def __str__(self) -> str:
+        result = "{ "
+        result += str(self.val)
+        result += " }"
 
 
 class LinkedListInterface:
@@ -30,56 +35,83 @@ class LinkedListInterface:
 
 
 class LinkedList(LinkedListInterface):
-    def __init__(self):
-        self.head = None
+    def __init__(self) -> None:
+        self.size = 0
+        self.first = None
+        self.last = None
 
     def append(self, node: Node) -> None:
-        if self.head is None:
-            self.head = node
-            return
-        cursor = self.head
-        while cursor.next is not None:
-            cursor = cursor.next
-        cursor.next = node
+        if self.size == 0:
+            self.first = node
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+        self.size += 1
+        return
 
     def pop(self) -> Node:
-        if self.head is None:
-            raise NoSuchElementException()
-        # 헤드에만 노드가 있는 경우
-        if self.head.next is None:
-            last_node = self.head
-            self.head = None
-            return last_node
-        node = self.head
-        while node.next.next is not None:
+        if self.size == 0:
+            raise NoSuchElement()
+        if self.size == 1:
+            node = self.first
+            self.first = None
+            self.last = None
+            self.size -= 1
+            return node
+        before_node = self.first
+        node = before_node.next
+        while node.next is not None:
+            before_node = before_node.next
             node = node.next
-        last_node = node.next
-        node.next = None
-        return last_node
+        before_node.next = None
+        self.last = before_node
+        self.size -= 1
+        return node
 
     def shift(self) -> Node:
-        if self.head is None:
-            return NoSuchElementException()
-        node = self.head
-        self.head = self.head.next
+        if self.size == 0:
+            raise NoSuchElement()
+        if self.size == 1:
+            node = self.first
+            self.first = None
+            self.last = None
+            self.size -= 1
+            return node
+        node = self.first
+        next_node = node.next
+        node.next = None
+        self.first = next_node
+        self.size -= 1
         return node
 
     def unshift(self, node: Node) -> None:
-        node.next = self.head
-        self.head = node
+        if self.size == 0:
+            self.first = node
+            self.last = node
+        else:
+            node.next = self.first
+            self.first = node
+        self.size += 1
+        return
 
-    def __str__(self):
-        current_node = self.head
-        if current_node is None:
-            return "{}"
-        output = "{"
-        output += str(current_node.val)
-        while current_node.next is not None:
-            current_node = current_node.next
-            output += " -> "
-            output += str(current_node.val)
-        output += "}"
-        return output
+    def __str__(self) -> str:
+        if self.size == 0:
+            return 'None'
+        node = self.first
+        result = ""
+        result += "{ "
+        result += str(node.val)
+        result += " }"
+        result += " -> "
+        while node.next is not None:
+            node = node.next
+            result += "{ "
+            result += str(node.val)
+            result += " }"
+            result += " -> "
+        result += "None"
+        return result
 
 
 linked_list = LinkedList()
