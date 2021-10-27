abbdf5f6003cbc97ff5a432d4cd823d213830223
parkchoongho <pchyo92@gmail.com>
2021-10-27 11:06:34 +0900

    리트코드 1번 문제 해결 방법 추가

--- leetcode/array/1_two_sum.py
+++ leetcode/array/1_two_sum.py
@@ -1,15 +1,36 @@
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: list, target: int):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        val_dict = {}
-        
-        for i in range(len(nums)):
-            target_num = target - nums[i]
-            if nums[i] in val_dict:
-                return [val_dict[nums[i]], i]
-            else: 
-                val_dict[target_num] = i
+        """
+        1. 첫번째 수를 뺀 결과 키 조회
+        """
+        # val_dict = {}
+
+        # for i in range(len(nums)):
+        #     target_num = target - nums[i]
+        #     if nums[i] in val_dict:
+        #         return [val_dict[nums[i]], i]
+        #     else:
+        #         val_dict[target_num] = i
+
+        """
+        2. 포인터 2개 활용        
+        """
+        copy_nums = nums[:]
+        copy_nums.sort()
+
+        left = 0
+
+        right = len(copy_nums) - 1
+
+        while left < right:
+            if (copy_nums[left] + copy_nums[right]) > target:
+                right -= 1
+            elif (copy_nums[left] + copy_nums[right]) < target:
+                left += 1
+            else:
+                return [nums.index(copy_nums[left]), nums.index(copy_nums[right])]
