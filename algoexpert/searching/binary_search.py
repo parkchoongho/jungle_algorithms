d48d6db2947de4f406122c0cdc25f57ac0d5aaae
parkchoongho <pchyo92@gmail.com>
2022-01-06 22:16:40 +0900

    Binary Search 문제 풀이

---
+++ algoexpert/searching/binary_search.py
@@ -0,0 +1,14 @@
+# from math import floor
+def binarySearch(array, target):
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] > target:
+            right = mid - 1
+        elif array[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return -1
